526e5d85079b1d4ded3c64b59da2fc1425cd0642
Nadia <rudi_777@ukr.net>
2018-08-05 21:47:38 +0300

    5.0

---
+++ src/main/java/com/tkach/FileEventLogger.java
@@ -0,0 +1,20 @@
+package com.tkach;
+
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileEventLogger implements EventLogger {
+    private String filename;
+
+    @Override
+    public void logEvent(Event event) {
+        try {
+            FileUtils.writeStringToFile(new File(filename), event.getMsg(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
